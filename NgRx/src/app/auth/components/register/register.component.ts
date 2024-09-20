cf11388557effab12660a585e0f33dd0346f5348
Simon <73712759+ZvSimon@users.noreply.github.com>
2024-09-20 18:02:25 +0200

    install ngrx store

--- NgRx/src/app/auth/components/register/register.component.ts
+++ NgRx/src/app/auth/components/register/register.component.ts
@@ -1,6 +1,9 @@
-import {Component} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {Component, inject} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {RouterLink} from "@angular/router";
+import {Store} from "@ngrx/store";
+import {register} from "../../store/action";
+import {RegisterRequestInterface} from "../../types/registerRequest.interface";
 
 @Component({
   selector: 'ngrx-register',
@@ -10,14 +13,26 @@ import {RouterLink} from "@angular/router";
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  form = new FormGroup({
-    username: new FormControl('', {validators: [Validators.required, Validators.minLength(6)]}),
-    email: new FormControl('', {validators: [Validators.required, Validators.minLength(6)]}),
-    password: new FormControl('', {validators: [Validators.required, Validators.required, Validators.minLength(6)]}),
+  store: Store
+  form: FormGroup
 
-  })
+  constructor() {
+    this.store = inject(Store)
+    this.form = new FormGroup({
+      username: new FormControl('', {validators: [Validators.required]}),
+      email: new FormControl('', {validators: [Validators.required]}),
+      password: new FormControl('', {validators: [Validators.required]}),
+    })
+  }
 
   onSubmit() {
     console.log('form', this.form.getRawValue())
+    if (this.form.invalid) {
+      return
+    }
+    const request: RegisterRequestInterface = {
+      user: this.form.getRawValue(),
+    }
+    this.store.dispatch(register({request}))
   }
 }
